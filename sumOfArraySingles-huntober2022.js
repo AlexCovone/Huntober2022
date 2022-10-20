39e6dfb7130552d62f25d69c7db3483fe948cd9b
Alex Covone <alexcovone@gmail.com>
2022-10-20 18:14:24 -0400

    Summed values that did not have duplicates in original array

---
+++ sumOfArraySingles-huntober2022.js
@@ -0,0 +1,10 @@
+/*
+You will be given an array of numbers in which two numbers occur once and the rest occur only twice. Your task will be to return the sum of the numbers that occur only once.
+
+For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15. Every other number occurs twice.
+*/
+
+function repeats(arr){
+    //indexOf num is lastIndexOf num
+    return arr.filter(element => arr.indexOf(element) === arr.lastIndexOf(element)).reduce((acc, c) => acc + c, 0)
+}
